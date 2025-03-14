dbcd8e429255a0b1ae6065b04212ac2b5a796bee
Stanislav Humeniuk <stanislav.humeniuk@outlook.com>
2025-03-14 23:30:26 +0200

    init commit

---
+++ src/client/java/sonormay/simplechainmail/SimpleChainmailClient.java
@@ -0,0 +1,10 @@
+package sonormay.simplechainmail;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleChainmailClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
